fe3b687924e969e0df3e7c23c1b4ca355e0f1840
[TatianaHolubenko] <tatiana.holubenko01@gmail.com>
2023-12-06 20:57:36 +0200

    add

--- js/task-06.js
+++ js/task-06.js
@@ -14,12 +14,12 @@ function getRandomHexColor() {
 function createBoxes(amount) {
   destroyBoxes();
 
-  let size = 30;
-  const moreOn = 10;
-  const array = [];
-
   const newAmount = Number(input.value);
   if (newAmount >= 1 && newAmount <= 100) {
+    let size = 30;
+    const moreOn = 10;
+    const array = [];
+
     for (let i = 0; i < amount; i++) {
       const newDiv = document.createElement('div');
       newDiv.style.backgroundColor = getRandomHexColor();
