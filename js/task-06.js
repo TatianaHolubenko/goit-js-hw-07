1b30723e98c97ac6a9fabbfd6816fb3bf1dd3487
[TatianaHolubenko] <tatiana.holubenko01@gmail.com>
2023-12-06 21:46:32 +0200

    add

--- js/task-06.js
+++ js/task-06.js
@@ -14,26 +14,23 @@ function getRandomHexColor() {
 function createBoxes(amount) {
   destroyBoxes();
 
-  const newAmount = Number(input.value);
-  if (newAmount >= 1 && newAmount <= 100) {
-    let size = 30;
-    const moreOn = 10;
-    const array = [];
+  let size = 30;
+  const moreOn = 10;
+  const array = [];
 
-    for (let i = 0; i < amount; i++) {
-      const newDiv = document.createElement('div');
-      newDiv.style.backgroundColor = getRandomHexColor();
-      newDiv.style.width = `${size}px`;
-      newDiv.style.height = `${size}px`;
+  for (let i = 0; i < amount; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.style.backgroundColor = getRandomHexColor();
+    newDiv.style.width = `${size}px`;
+    newDiv.style.height = `${size}px`;
 
-      array.push(newDiv);
-      size += moreOn;
-    }
-
-    boxes.append(...array);
-    input.value = '';
-    console.log(`Create, ${amount}`);
+    array.push(newDiv);
+    size += moreOn;
   }
+
+  boxes.append(...array);
+
+  console.log(`Create, ${amount}`);
 }
 
 function destroyBoxes() {
@@ -41,5 +38,11 @@ function destroyBoxes() {
   console.log('Destroy');
 }
 
-buttonCreate.addEventListener('click', createBoxes);
+buttonCreate.addEventListener('click', () => {
+  const newAmount = Number(input.value);
+  if (newAmount >= 1 && newAmount <= 100) {
+    createBoxes(newAmount);
+  }
+});
+
 buttonDestroy.addEventListener('click', destroyBoxes);
